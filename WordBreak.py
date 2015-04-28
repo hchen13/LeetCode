5700449da1e5656bf6d8c7776fb14fad172da556
Ethan Chan <lemonchen.us@gmail.com>
2015-04-28 17:57:05 +0800

    undated wordbreak.py, problem is now solved.

--- WordBreak.py
+++ WordBreak.py
@@ -1,6 +1,19 @@
 class Solution:
-	# @param s, a string
-	# @param wordDict, a set<string>
-	# @return a boolean
-	def wordBreak(self, s, wordDict):
-		size = len(s)
+    # @param s, a string
+    # @param wordDict, a set<string>
+    # @return a boolean
+    def wordBreak(self, s, wordDict):
+        size = len(s)
+        f = [False for i in range(size + 1)]
+        f[0] = True
+        stack = []
+        for i in range(size+1):
+            for j in range(i):
+                if f[j] is True and s[j: i] in wordDict:
+                    f[i] = True
+                    stack.append(s[j: i])
+        return f[size]
+
+s = Solution()
+print s.wordBreak("leetcodeleet", ["lee", "t", "code"])
+        
